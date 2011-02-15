eda24c6cecc1ff48459b700c00544f9f50820bd6
Tim Jones <github@roastedamoeba.com>
2011-02-15 23:20:27 +0800

    Better (well, simpler) fitting to bounds

--- src/Nexus/Graphics/Cameras/PerspectiveCamera.cs
+++ src/Nexus/Graphics/Cameras/PerspectiveCamera.cs
@@ -31,65 +31,16 @@ namespace Nexus.Graphics.Cameras
 		public static PerspectiveCamera CreateFromBounds(AxisAlignedBoundingBox bounds, Viewport viewport,
 			float fieldOfView, float yaw, float pitch, float zoom)
 		{
-			// Calculate initial guess at camera settings.
 			Matrix3D transform = Matrix3D.CreateFromYawPitchRoll(yaw, pitch, 0);
 			Vector3D cameraDirection = Vector3D.Normalize(transform.Transform(Vector3D.Forward));
-			PerspectiveCamera initialGuess = new PerspectiveCamera
-			{
-				FieldOfView = fieldOfView,
-				NearPlaneDistance = 1.0f,
-				FarPlaneDistance = bounds.Size.Length() * 10,
-				Position = bounds.Center - cameraDirection * bounds.Size.Length() * 3,
-				LookDirection = cameraDirection,
-				UpDirection = Vector3D.Up
-			};
-
-			Matrix3D projection = initialGuess.GetProjectionMatrix(viewport.AspectRatio);
-			Matrix3D view = initialGuess.GetViewMatrix();
-
-			// Project bounding box corners onto screen, and calculate screen bounds.
-			float closestZ = float.MaxValue;
-			Box2D? screenBounds = null;
-			Point3D[] corners = bounds.GetCorners();
-			foreach (Point3D corner in corners)
-			{
-				Point3D screenPoint = viewport.Project(corner,
-					projection, view, Matrix3D.Identity);
-
-				if (screenPoint.Z < closestZ)
-					closestZ = screenPoint.Z;
-
-				IntPoint2D intScreenPoint = new IntPoint2D((int) screenPoint.X, (int) screenPoint.Y);
-				if (screenBounds == null)
-					screenBounds = new Box2D(intScreenPoint, intScreenPoint);
-				else
-				{
-					Box2D value = screenBounds.Value;
-					value.Expand(intScreenPoint);
-					screenBounds = value;
-				}
-			}
-
-			// Now project back from screen bounds into scene, setting Z to the minimum bounding box Z value.
-			Point3D min = viewport.Unproject(new Point3D(screenBounds.Value.Min.X, screenBounds.Value.Min.Y, closestZ),
-				projection, view, Matrix3D.Identity);
-			Point3D max = viewport.Unproject(new Point3D(screenBounds.Value.Max.X, screenBounds.Value.Max.Y, closestZ),
-				projection, view, Matrix3D.Identity);
-
-			// Use these new values to calculate the distance the camera should be from the AABB centre.
-			Vector3D size = Vector3D.Abs(max - min);
-			float largestDimensionSize = Math.Max(size.X, Math.Max(size.Y, size.Z));
-			float dist = largestDimensionSize / (2 * MathUtility.Tan(fieldOfView / 2));
-			Point3D closestBoundsCenter = (Point3D) min + ((max - min) / 2);
-			Point3D position = closestBoundsCenter
-				- cameraDirection * dist * (1 / zoom);
-
+			AxisAlignedBoundingBox transformedBounds = bounds.Transform(transform);
+			float maxWidth = Math.Max(transformedBounds.Size.X, Math.Max(transformedBounds.Size.Y, transformedBounds.Size.Z));
 			return new PerspectiveCamera
 			{
 				FieldOfView = fieldOfView,
 				NearPlaneDistance = 1.0f,
-				FarPlaneDistance = dist * 10,
-				Position = position,
+				FarPlaneDistance = transformedBounds.Size.Length() * 10,
+				Position = bounds.Center - cameraDirection * maxWidth / zoom,
 				LookDirection = cameraDirection,
 				UpDirection = Vector3D.Up
 			};
